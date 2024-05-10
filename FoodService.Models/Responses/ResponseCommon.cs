797ab753e43dd7a6236d6ba08a5aeb633fb92ddb
Nicolas Paiuca Buscarini <nicolasbuscarini@hotmail.com>
2024-05-10 13:44:32 -0300

    FIX: Removing ResponseError

--- FoodService.Models/Responses/ResponseCommon.cs
+++ FoodService.Models/Responses/ResponseCommon.cs
@@ -4,17 +4,40 @@
     /// Represents a common response with generic data.
     /// </summary>
     /// <typeparam name="T">Type of the data.</typeparam>
-    public class ResponseCommon<T> : ResponseError
+    public class ResponseCommon<T>
     {
+        /// <summary>
+        /// Gets the status code of the response.
+        /// </summary>
+        public int StatusCode { get; private set; }
+
+        /// <summary>
+        /// Gets a value indicating whether the response is successful.
+        /// </summary>
+        public bool IsSuccess { get; private set; }
+
+        /// <summary>
+        /// Gets the message associated with the response.
+        /// </summary>
+        public string Message { get; private set; }
+
+        /// <summary>
+        /// Gets the error message associated with the response, if any.
+        /// </summary>
+        public string? Error { get; private set; }
 
         /// <summary>
         /// Gets the data associated with the response.
         /// </summary>
         public T Data { get; private set; }
 
-        public ResponseCommon(int statusCode, string message, T data, string? error = null, bool success = false) : base(statusCode, success, message, error)
+        private ResponseCommon(int statusCode, string message, T data, string? error = null, bool success = false)
         {
+            StatusCode = statusCode;
+            Message = message;
+            Error = error;
             Data = data;
+            IsSuccess = success;
         }
 
         /// <summary>
