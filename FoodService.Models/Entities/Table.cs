740a1047f96d05b8712de58200f8d53d42aa5c8f
Rafael-Oliveira-Gomes <rafinha.gomo.11.og@gmail.com>
2024-05-11 15:17:47 -0300

    Create model table

---
+++ FoodService.Models/Entities/Table.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodService.Models.Entities
+{
+    public class Table
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+        public List<Order> Orders { get; set; }
+    }
+}
