91d667bf374918b6be32e52206ee789bcea3a2b0
Nicolas Paiuca Buscarini <nicolasbuscarini@hotmail.com>
2024-08-28 17:04:42 -0300

    FEAT: OrderDto

---
+++ FoodService.Models/Dto/OrderDto.cs
@@ -0,0 +1,12 @@
+﻿using FoodService.Models.Entities;
+
+namespace FoodService.Models.Dto
+{
+    public class OrderDto
+    {
+        /// <summary>
+        /// Gets or sets the list of items included in the order.
+        /// </summary>
+        public List<OrderItem> OrderItems { get; set; }
+    }
+}
