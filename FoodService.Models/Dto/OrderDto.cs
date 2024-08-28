ffe315b8a3c82c2b3218e7a7a6146a375f70ed3d
Nicolas Paiuca Buscarini <nicolasbuscarini@hotmail.com>
2024-08-28 17:07:15 -0300

    FEAT: OrderItemDto

--- FoodService.Models/Dto/OrderDto.cs
+++ FoodService.Models/Dto/OrderDto.cs
@@ -7,6 +7,23 @@ namespace FoodService.Models.Dto
         /// <summary>
         /// Gets or sets the list of items included in the order.
         /// </summary>
-        public List<OrderItem> OrderItems { get; set; }
+        public List<OrderItemDto> OrderItems { get; set; }
+    }
+    public class OrderItemDto
+    {
+        /// <summary>
+        /// Gets or sets the quantity of the product in the order item.
+        /// </summary>
+        public int Quantity { get; set; }
+
+        /// <summary>
+        /// Gets or sets any additional comments for the order item.
+        /// </summary>
+        public string? Comment { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ID of the product associated with this order item.
+        /// </summary>
+        public int ProductId { get; set; }
     }
 }
